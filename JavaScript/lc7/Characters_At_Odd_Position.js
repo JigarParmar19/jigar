b60bc50fc6b834eb5f2f0eee1fa0466ecf806384
JigarParmar19 <153288821+JigarParmar19@users.noreply.github.com>
2024-05-01 11:41:37 +0530

    Add files via upload

---
+++ JavaScript/lc7/Characters_At_Odd_Position.js
@@ -0,0 +1,10 @@
+let Str = "white";
+let arr = '';
+
+for (let i = 1; i < Str.length; i += 2)
+ {
+    arr += Str[i];
+}
+
+console.log(arr); 
+                
---
+++ JavaScript/lc7/count_vowel.js
@@ -0,0 +1,1 @@
+console.log("red and white".length); 
