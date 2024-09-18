52e3aaee35bcb8b24b8938e1ddc89f76c97c071c
JigarParmar19 <153288821+JigarParmar19@users.noreply.github.com>
2024-09-18 19:14:36 +0530

    Add files via upload

---
+++ DSA/exam-6/q3.js
@@ -0,0 +1,15 @@
+const getsum = (arr) =>{
+
+    let sum = 0;
+
+    for(let i = 0; i < arr.length; i++){
+        sum += arr[i];
+    }
+  
+    return sum;
+}
+
+let arr = [1,2,3,4];
+
+console.log(getsum(arr));
+
